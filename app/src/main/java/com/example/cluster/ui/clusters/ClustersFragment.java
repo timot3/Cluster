c1d6a35456ae1a6a85a7bc0f912c5899464eede3
jmahajan117 <jay.mahajan33@gmail.com>
2020-08-08 16:10:31 -0400

    Finished code to get firebase name's of clusters. Working to get cluster objects

--- app/src/main/java/com/example/cluster/ui/clusters/ClustersFragment.java
+++ app/src/main/java/com/example/cluster/ui/clusters/ClustersFragment.java
@@ -1,6 +1,7 @@
 package com.example.cluster.ui.clusters;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,23 +10,21 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.example.cluster.Lobby;
 import com.example.cluster.R;
 import com.example.cluster.StudentViewCluster;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClustersFragment extends Fragment {
@@ -34,45 +33,54 @@ public class ClustersFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         clustersViewModel =
                 ViewModelProviders.of(this).get(ClustersViewModel.class);
         View root = inflater.inflate(R.layout.fragment_clusters, container, false);
 
         //Values that we will get from firebase user
-        String[] values = {"PHYS 214", "CS 125", "eSports 360 (Owner)"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                getContext(), android.R.layout.simple_list_item_1, values);
-
-        //Setting adapter
-        ListView listView = (ListView) root.findViewById(R.id.lstMain);
-        listView.setAdapter(adapter);
-
         FirebaseFirestore.getInstance()
                 .collection("clusters")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        ArrayList<String> values = new ArrayList<>();
                         if (task.isSuccessful()) {
                             List<DocumentSnapshot> myListOfDocuments = task.getResult().getDocuments();
                             for (int i = 0; i < myListOfDocuments.size(); i++) {
-                                String clusterName = myListOfDocuments.get(i).getString("name");
-                                Log.wtf("bruh", clusterName);
+                                String name = myListOfDocuments.get(i).getString("name");
+                                values.add(name);
                             }
+
+                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                                    getContext(), android.R.layout.simple_list_item_1, values);
+
+                            //Setting adapter
+                            ListView listView = (ListView) root.findViewById(R.id.lstMain);
+                            listView.setAdapter(adapter);
+
+                            //Setting items to be clickable
+                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView <? > arg0, View view, int position, long id) {
+                                    Intent i = new Intent(getActivity(), StudentViewCluster.class);
+                                    i.putExtra("Cluster", values.get(position));
+                                    startActivity(i);
+                                }
+
+                            });
+
                         }
                     }
                 });
 
-        //Setting items to be clickable
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView <? > arg0, View view, int position, long id) {
-                Intent i = new Intent(getActivity(), StudentViewCluster.class);
-                i.putExtra("Cluster", values[position]);
-                startActivity(i);
-            }
+        String[] clusters = {"PHYS 214", "CS 125", "eSports 360 (Owner)"};
 
-        });
         return root;
     }
+
+
+
+
 }
