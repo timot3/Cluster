2641f38a82d5eb4ea7fb811a6e44e85a9d9dcfae
jmahajan117 <jay.mahajan33@gmail.com>
2020-08-07 22:59:51 -0400

    Add UI elements in Login Screen

--- app/src/main/java/com/example/cluster/MainActivity.java
+++ app/src/main/java/com/example/cluster/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.EditText;
+
+import java.sql.Connection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSignInClick(View v) {
+
     }
 }
