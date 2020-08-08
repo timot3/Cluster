facdb7d3c22348b43f5047222180ef2e8c2061f7
menezes4 <menezes4@illinois.edu>
2020-08-07 22:31:42 -0700

    Started on new user authentication.

--- app/src/main/java/com/example/cluster/CreateNewUser.java
+++ app/src/main/java/com/example/cluster/CreateNewUser.java
@@ -3,12 +3,27 @@ package com.example.cluster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class CreateNewUser extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_user);
+
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void onClickFormatNewUser(View v) {
+        
     }
 }
