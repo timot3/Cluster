2a4243863031d44f820bb2cd84ea8b3034279c8f
jmahajan117 <jay.mahajan33@gmail.com>
2020-08-08 17:27:21 -0400

    Progress on creating a cluster, still not able to use function right now

--- app/src/main/java/com/example/cluster/ui/create/CreateFragment.java
+++ app/src/main/java/com/example/cluster/ui/create/CreateFragment.java
@@ -1,10 +1,12 @@
 package com.example.cluster.ui.create;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,9 +17,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cluster.R;
 import com.example.cluster.ui.join.JoinViewModel;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.HttpsCallableResult;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateFragment extends Fragment {
     private CreateViewModel createViewModel;
+    private FirebaseFunctions functions;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,14 +44,42 @@ public class CreateFragment extends Fragment {
 
 
         Button button = root.findViewById(R.id.createButton);
+        final String clusterName = ((EditText) root.findViewById(R.id.createField)).getText().toString();
+        final String code = ((EditText) root.findViewById(R.id.codeField)).getText().toString();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (code.isEmpty() || clusterName.isEmpty()) {
+                    //Send Invalid here
+                    return;
+                }
 
+                else {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("active", true);
+                    data.put("name", clusterName);
+                    data.put("password", code);
+                    addCluster(data);
+                }
             }
         });
 
         return root;
+
+    }
+
+    private Task<String> addCluster(Map<String, Object> data) {
+        return functions
+                .getHttpsCallable("createCluster")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, String>() {
+                    @Override
+                    public String then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        String result = (String) task.getResult().getData();
+                        Log.wtf("YEET", result);
+                        return result;
+                    }
+                });
 
     }
 
