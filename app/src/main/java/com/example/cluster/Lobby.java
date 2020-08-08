7564422179daa5b3da8f491c0aeb225ff3d4f9c3
Gaurav Raj <graj@tibco.com>
2020-08-07 23:26:28 -0700

    Nav buttons work

--- app/src/main/java/com/example/cluster/Lobby.java
+++ app/src/main/java/com/example/cluster/Lobby.java
@@ -29,7 +29,7 @@ public class Lobby extends AppCompatActivity {
 
         //ADD FRAGMENTS HERE
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,
+                R.id.navigation_clusters, R.id.navigation_join, R.id.navigation_create,
                 R.id.settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
--- app/src/main/java/com/example/cluster/ui/dashboard/JoinViewModel.java
+++ app/src/main/java/com/example/cluster/ui/dashboard/JoinViewModel.java
@@ -1,16 +1,16 @@
-package com.example.cluster.ui.home;
+package com.example.cluster.ui.dashboard;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class JoinViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public JoinViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is dashboard fragment");
     }
 
     public LiveData<String> getText() {
