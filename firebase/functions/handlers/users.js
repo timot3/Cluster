cd40576ec49d4c22e8b17de9e02616f3146e53bd
s-hfarooq <1hassanfarooq@gmail.com>
2020-08-08 12:04:40 -0700

    Updated signup function

--- firebase/functions/handlers/users.js
+++ firebase/functions/handlers/users.js
@@ -5,62 +5,12 @@ const { createRandID, validateSignUpData } = require('../helpers/helper');
 
 firebase.initializeApp(config);
 
-// exports.signup = (req, res) => {
-//     const newUser = {
-//         name: req.body.name,
-//         email: req.body.email,
-//         password: req.body.password,
-//         confirmPassword: req.body.confirmPassword,
-//     };
-//
-//     // const { valid, errors } = validateSignUpData(newUser);
-//     // if(!valid)
-//     //     return res.status(400).json(errors);
-//
-//     let token, userId;
-//     //var randID = createRandID();
-//     var randID = "eruigheriugherisughuersghesirughuersg"
-//
-//     db.doc(`/users/${randID}`).get().then(doc => {
-//         if(doc.exists) {
-//             return res.status(400).json({ handle: 'this handle is already taken' });
-//         } else {
-//             return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
-//         }
-//     }).then(data => {
-//         userId = data.user.uid;
-//         return data.user.getIdToken();
-//     }).then(idToken => {
-//         token = idToken;
-//         const userCredentials = {
-//             email: newUser.email,
-//             name: newUser.name,
-//             clusters: [],
-//             ownerOf: [],
-//         };
-//
-//         return db.doc(`/users/${randID}`).set(userCredentials);
-//     }).then(() => {
-//         return res.status(201).json({ token });
-//     }).catch(err => {
-//         console.error(err);
-//         if(err.code === 'auth/email-already-in-use') {
-//             return res.status(400).json({ email: 'Email is already in use' })
-//         } else {
-//             return res.status(500).json({ error: err.code });
-//         }
-//     });
-// };
-
-
-
 exports.signup = (req, res) => {
     const newUser = {
-        firstName: req.body.firstName,
+        name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
-        handle: req.body.handle,
+        //confirmPassword: req.body.confirmPassword,
     };
 
     // const { valid, errors } = validateSignUpData(newUser);
@@ -68,8 +18,9 @@ exports.signup = (req, res) => {
     //     return res.status(400).json(errors);
 
     let token, userId;
+    //let randName = createRandID();
 
-    db.doc(`/users/${newUser.handle}`).get().then(doc => {
+    db.doc(`/users/${req.body.email}`).get().then(doc => {
         if(doc.exists) {
             return res.status(400).json({ handle: 'this handle is already taken' });
         } else {
@@ -80,15 +31,15 @@ exports.signup = (req, res) => {
         return data.user.getIdToken();
     }).then(idToken => {
         token = idToken;
-        const userCcredentials = {
-            handle: newUser.handle,
+        const userCredentials = {
             email: newUser.email,
-            firstName: newUser.firstName,
-            createdAt: new Date().toISOString(),
+            name: newUser.name,
+            clusters: [],
+            ownerOf: [],
             userId
         };
 
-        return db.doc(`/users/${newUser.handle}`).set(userCcredentials);
+        return db.doc(`/users/${req.body.email}`).set(userCredentials);
     }).then(() => {
         return res.status(201).json({ token });
     }).catch(err => {
