288ef6ac6993fe564940f3e0c3191768fe408c1c
Timothy Vitkin <timothy.vitkin@gmail.com>
2020-08-09 22:13:33 -0400

    Use Glide library for tabletennis loading animation

--- app/src/main/java/com/example/cluster/StudentViewCluster.java
+++ app/src/main/java/com/example/cluster/StudentViewCluster.java
@@ -4,15 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
 
 public class StudentViewCluster extends AppCompatActivity {
+    private ImageView loadingGif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_view_cluster);
         Intent intent = getIntent();
         String title = intent.getStringExtra("Cluster");
         setTitle(title);
+
+        //Use Glide library for custom loading animation
+        loadingGif = (ImageView) findViewById(R.id.student_view_loading_gif);
+        Glide.with(this).load(R.drawable.tabletennis).into(loadingGif);
+
     }
 }
