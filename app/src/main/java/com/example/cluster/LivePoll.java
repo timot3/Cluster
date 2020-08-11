ae120b7d52320069c5390c51bf8853beda380f8f
jmahajan117 <jay.mahajan33@gmail.com>
2020-08-11 17:56:37 -0400

    Progress on LivePoll class, working on getting live data and switching fragments

--- app/src/main/java/com/example/cluster/LivePoll.java
+++ app/src/main/java/com/example/cluster/LivePoll.java
@@ -1,15 +1,46 @@
 package com.example.cluster;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class LivePoll extends AppCompatActivity {
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+public class LivePoll extends FragmentActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_poll);
         setTitle("Live Poll");
+
+        Fragment fragment = new WaitingFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainer, fragment).commit();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+
 }
