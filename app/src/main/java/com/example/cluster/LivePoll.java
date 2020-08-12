516d4910f65fc9feef7c2d367f1934d3716155a2
jmahajan117 <jay.mahajan33@gmail.com>
2020-08-11 22:00:31 -0400

    FIGURED OUT FRAGMENT TRANSITION HOLY CRAP

--- app/src/main/java/com/example/cluster/LivePoll.java
+++ app/src/main/java/com/example/cluster/LivePoll.java
@@ -1,28 +1,27 @@
 package com.example.cluster;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
-import android.view.View;
 
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class LivePoll extends FragmentActivity {
+
+
+
+    Handler handler = new Handler();
+    Fragment fragment;
 
 
     @Override
@@ -31,16 +30,46 @@ public class LivePoll extends FragmentActivity {
         setContentView(R.layout.activity_live_poll);
         setTitle("Live Poll");
 
-        Fragment fragment = new WaitingFragment();
+        fragment = new WaitingFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragmentContainer, fragment).commit();
+
+
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                FirebaseFirestore.getInstance().collection("clusters")
+                        .document(getIntent().getStringExtra("ID"))
+                        .collection("livepolls")
+                        .document("live").get().addOnCompleteListener(task -> {
+                            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    if (task.isComplete()) {
+                        DocumentSnapshot snapshot = task.getResult();
+                        String question = snapshot.getString("question");
+                        if (!(question.isEmpty())) {
+                            //Add fragment
+                            try {
+                                fragmentTransaction.replace(R.id.fragmentContainer,
+                                        new QuestionFragment());
+                                fragmentTransaction.commit();
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        else {
+                            Log.wtf("Bruh", "This ain't it");
+                            handler.postDelayed(this, 1000);
+                        }
+                    }
+                });
+            }
+
+        }, 1000);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
 
-    }
 
 
 }
