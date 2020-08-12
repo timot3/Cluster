bb7370688181de020f594cb7478be4721178056e
jmahajan117 <jay.mahajan33@gmail.com>
2020-08-12 12:11:04 -0400

    Created UI for PromptFragment

--- app/src/main/java/com/example/cluster/PromptFragment.java
+++ app/src/main/java/com/example/cluster/PromptFragment.java
@@ -1,6 +1,8 @@
 package com.example.cluster;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,12 @@ public class PromptFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_prompt, container, false);
+
+
+        Intent thisScreen = getActivity().getIntent();
+        String clusterID = thisScreen.getStringExtra("ID");
+
+
         return root;
     }
 }
