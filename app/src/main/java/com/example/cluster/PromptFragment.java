a2ec60eb700fd473106505621a8a77630966fd74
jmahajan117 <jay.mahajan33@gmail.com>
2020-08-12 18:00:20 -0400

    Made the ability for teachers to post question live, work on adding daily posts as well

--- app/src/main/java/com/example/cluster/PromptFragment.java
+++ app/src/main/java/com/example/cluster/PromptFragment.java
@@ -6,10 +6,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class PromptFragment extends Fragment {
 
@@ -25,6 +33,19 @@ public class PromptFragment extends Fragment {
         Intent thisScreen = getActivity().getIntent();
         String clusterID = thisScreen.getStringExtra("ID");
 
+        EditText questionText = (EditText) root.findViewById(R.id.questionView);
+        Button submit = (Button) root.findViewById(R.id.submitButton);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseFirestore.getInstance().collection("clusters")
+                        .document(clusterID).collection("livepolls")
+                        .document("live").update("question",
+                        questionText.getText().toString());
+                Toast.makeText(getContext(), "Question Posted", Toast.LENGTH_LONG).show();
+            }
+        });
 
         return root;
     }
