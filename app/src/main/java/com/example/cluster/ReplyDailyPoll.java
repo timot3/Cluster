2a3685f1016dd1bb3720b8ba22d21abbafaa0733
jmahajan117 <jay.mahajan33@gmail.com>
2020-08-13 17:57:51 -0400

    Gave the ability for students to reply to daily polls

--- app/src/main/java/com/example/cluster/ReplyDailyPoll.java
+++ app/src/main/java/com/example/cluster/ReplyDailyPoll.java
@@ -1,8 +1,23 @@
 package com.example.cluster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReplyDailyPoll extends AppCompatActivity {
 
@@ -10,5 +25,27 @@ public class ReplyDailyPoll extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reply_daily_poll);
+        Intent thisScreen = getIntent();
+        setTitle(thisScreen.getStringExtra("title"));
+    }
+
+    public void onSubmit(View view) {
+        String reply = ((EditText) findViewById(R.id.replyField)).getText().toString();
+
+
+        if (!(reply.isEmpty())) {
+            FirebaseFirestore.getInstance().collection("clusters")
+                    .document(getIntent().getStringExtra("clusterID"))
+                    .collection("dailypolls")
+                    .document(getIntent().getStringExtra("pollID")).update("replies",
+                    FieldValue.arrayUnion(reply)).addOnCompleteListener(task -> {
+                        if (task.isComplete()) {
+                            Toast.makeText(this, "Reply to " + getIntent().getStringExtra("title")
+                                    + " Sucessfull", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+            finish();
+        }
     }
 }
