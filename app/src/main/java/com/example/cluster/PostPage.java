9588a5305cdc99c187544e50c76abd7112ac4cc6
jmahajan117 <jay.mahajan33@gmail.com>
2020-08-13 02:21:42 -0400

    Working on post UI

--- app/src/main/java/com/example/cluster/PostPage.java
+++ app/src/main/java/com/example/cluster/PostPage.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -18,6 +20,10 @@ public class PostPage extends AppCompatActivity {
         setContentView(R.layout.activity_post_page);
         Intent thisPage = getIntent();
         setTitle(thisPage.getStringExtra("title"));
+
+        TextView title = (TextView) findViewById(R.id.titleView);
+        title.setText(thisPage.getStringExtra("title"));
+        ListView listView = (ListView) findViewById(R.id.replyList);
     }
 
 }
