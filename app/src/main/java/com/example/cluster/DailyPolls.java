8193a8ffa8c45024ad5898f413ab89adc974a190
Timothy Vitkin <timothy.vitkin@gmail.com>
2020-08-12 22:50:21 -0400

    Begin UI changes

--- app/src/main/java/com/example/cluster/DailyPolls.java
+++ app/src/main/java/com/example/cluster/DailyPolls.java
@@ -2,9 +2,10 @@ package com.example.cluster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class DailyPolls extends AppCompatActivity {
+public class DailyPolls extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
